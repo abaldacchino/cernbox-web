b175daa9d17e2730fbdda16591999c29dd9d3491
Adriana Baldacchino <adriana.baldacchino@cern.ch>
2023-08-22 09:15:00 +0200

    Disabiling import action

--- packages/web-app-files/src/components/FilesList/ContextActions.vue
+++ packages/web-app-files/src/components/FilesList/ContextActions.vue
@@ -9,7 +9,7 @@ import FileActions from '../../mixins/fileActions'
 import CreateQuicklink from '../../mixins/actions/createQuicklink'
 import EmptyTrashBin from '../../mixins/actions/emptyTrashBin'
 import ExportOverleaf from '../../mixins/actions/exportOverleaf'
-import ImportOverleaf from '../../mixins/actions/importOverleaf'
+// import ImportOverleaf from '../../mixins/actions/importOverleaf'
 import Paste from '../../mixins/actions/paste'
 import ShowActions from '../../mixins/actions/showActions'
 import ShowDetails from '../../mixins/actions/showDetails'
@@ -37,8 +37,8 @@ export default {
     SetSpaceImage,
     SetSpaceReadme,
     SpaceNavigate,
-    ExportOverleaf,
-    ImportOverleaf
+    ExportOverleaf
+    // ImportOverleaf
   ],
 
   props: {
@@ -141,7 +141,7 @@ export default {
     menuItemsActions() {
       return [
         ...this.$_exportToOverleaf,
-        ...this.$_importFromOverleaf,
+        // ...this.$_importFromOverleaf,
         ...this.$_downloadArchive_items,
         ...this.$_downloadFile_items,
         ...this.$_delete_items,
